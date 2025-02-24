8f70a3f66d75d6c5f99333e0225e76d9211ecf6c
00silun <80539080+00silun@users.noreply.github.com>
2025-02-24 23:22:57 +0100

    Update sensor.py

--- components/custom_ec_sensor/sensor.py
+++ components/custom_ec_sensor/sensor.py
@@ -9,7 +9,9 @@ custom_ec_sensor_ns = cg.esphome_ns.namespace("custom_ec_sensor")
 EcSensor = custom_ec_sensor_ns.class_("EcSensor", cg.PollingComponent)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(EcSensor)
+    cv.GenerateID(): cv.declare_id(EcSensor),
+    cv.Required("ads_sensor"): cv.use_id(sensor.Sensor),
+    cv.Required("water_temperature"): cv.use_id(sensor.Sensor)
     
 }).extend(cv.polling_component_schema('10s'))
 
@@ -17,6 +19,9 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield sensor.register_sensor(var, config)
+
+
+
 
 
 
