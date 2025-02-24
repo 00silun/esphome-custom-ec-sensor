1d1fe514c7a337d9cc281da3e110a996d96e46ea
00silun <80539080+00silun@users.noreply.github.com>
2025-02-24 22:15:20 +0100

    Update sensor.py

--- components/sensor.py
+++ components/sensor.py
@@ -1,17 +1,22 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
-
-# Delay import of the sensor module to avoid circular imports.
-sensor_component = __import__("esphome.components.sensor", fromlist=["Sensor"])
+from esphome.components import sensor
+from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY
 
 custom_ec_sensor_ns = cg.esphome_ns.namespace("custom_ec_sensor")
 
 # Declare your C++ class using the dynamically imported sensor module.
-EcSensor = custom_ec_sensor_ns.class_("EcSensor", cg.PollingComponent, sensor_component.Sensor)
+EcSensor = custom_ec_sensor_ns.class_("EcSensor", cg.PollingComponent)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(EcSensor),
-    cv.Required("ads_sensor"): cv.use_id(sensor_component.Sensor),
-    cv.Required("water_temperature"): cv.use_id(sensor_component.Sensor),
-}).extend(sensor_component.sensor_schema(EcSensor))
+    cv.GenerateID(): cv.declare_id(EcSensor)
+    
+}).extend(sensor_component.sensor_schema('10s'))
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield sensor.register_sensor(var, config)
+
+
+
