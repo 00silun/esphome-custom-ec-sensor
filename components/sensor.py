cd178162aa4c60ef7b7af443c18f66d6cbb18e5d
00silun <80539080+00silun@users.noreply.github.com>
2025-02-24 22:01:25 +0100

    Update sensor.py

--- components/sensor.py
+++ components/sensor.py
@@ -1,13 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor as sensor_component
 from esphome.const import CONF_ID
 
-# Create a namespace matching your C++ code.
-custom_ec_sensor_ns = cg.esphome_ns.namespace('custom_ec_sensor')
+# Delay import of the sensor module to avoid circular imports.
+sensor_component = __import__("esphome.components.sensor", fromlist=["Sensor"])
 
-# Declare your C++ class. Make sure "EcSensor" exactly matches your C++ class name.
-EcSensor = custom_ec_sensor_ns.class_('EcSensor', cg.PollingComponent, sensor_component.Sensor)
+custom_ec_sensor_ns = cg.esphome_ns.namespace("custom_ec_sensor")
+
+# Declare your C++ class using the dynamically imported sensor module.
+EcSensor = custom_ec_sensor_ns.class_("EcSensor", cg.PollingComponent, sensor_component.Sensor)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(EcSensor),
