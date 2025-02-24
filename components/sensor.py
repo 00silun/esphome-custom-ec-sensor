3192e5fe010ec121a09c27cc4ee4a8f04b89d678
00silun <80539080+00silun@users.noreply.github.com>
2025-02-24 21:49:05 +0100

    Update sensor.py

--- components/sensor.py
+++ components/sensor.py
@@ -1,16 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor as sensor_component
+from esphome.components import sensor
 from esphome.const import CONF_ID
 
 # Create a namespace matching your C++ code.
 custom_ec_sensor_ns = cg.esphome_ns.namespace('custom_ec_sensor')
 
 # Declare your C++ class. Ensure "EcSensor" exactly matches your header.
-EcSensor = custom_ec_sensor_ns.class_('EcSensor', cg.PollingComponent, sensor_component.sensor)
+EcSensor = custom_ec_sensor_ns.class_('EcSensor', cg.PollingComponent, sensor.sensor)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(EcSensor),
-    cv.Required("ads_sensor"): cv.use_id(sensor_component.sensor),
-    cv.Required("water_temperature"): cv.use_id(sensor_component.sensor),
-}).extend(sensor_component.sensor_schema(EcSensor))
+    cv.Required("ads_sensor"): cv.use_id(sensor.sensor),
+    cv.Required("water_temperature"): cv.use_id(sensor.sensor),
+}).extend(sensor.sensor.sensor_schema(EcSensor))
