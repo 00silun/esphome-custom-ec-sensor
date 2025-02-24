6871657b7d452046461e00dfe50e40067e703674
00silun <80539080+00silun@users.noreply.github.com>
2025-02-24 23:27:41 +0100

    Update sensor.py

--- components/custom_ec_sensor/sensor.py
+++ components/custom_ec_sensor/sensor.py
@@ -6,14 +6,14 @@ from esphome.const import CONF_ID
 custom_ec_sensor_ns = cg.esphome_ns.namespace("custom_ec_sensor")
 
 # Declare your C++ class 
-EcSensor = custom_ec_sensor_ns.class_("EcSensor", cg.PollingComponent)
+EcSensor = custom_ec_sensor_ns.class_("EcSensor", cg.PollingComponent, sensor.Sensor)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(EcSensor),
     cv.Required("ads_sensor"): cv.use_id(sensor.Sensor),
     cv.Required("water_temperature"): cv.use_id(sensor.Sensor)
     
-}).extend(cv.polling_component_schema('10s'))
+}).extend(sensor.sensor_schema(EcSensor))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -22,6 +22,18 @@ def to_code(config):
 
 
 
+
+# Declare your C++ class with both polling and sensor bases.
+EcSensor = custom_ec_sensor_ns.class_(
+    "EcSensor", cg.PollingComponent, sensor_component.Sensor
+)
+
+# Extend the configuration with the default sensor schema.
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(EcSensor),
+    cv.Required("ads_sensor"): cv.use_id(sensor_component.Sensor),
+    cv.Required("water_temperature"): cv.use_id(sensor_component.Sensor),
+}).extend(sensor_component.sensor_schema(EcSensor))
 
 
 
