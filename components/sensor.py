460b56a8434c55c7c43d1055436edafe281debb5
00silun <80539080+00silun@users.noreply.github.com>
2025-02-24 22:17:25 +0100

    Update sensor.py

--- components/sensor.py
+++ components/sensor.py
@@ -11,7 +11,7 @@ EcSensor = custom_ec_sensor_ns.class_("EcSensor", cg.PollingComponent)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(EcSensor)
     
-}).extend(sensor.sensor_schema('10s'))
+}).extend(cv.polling_component_schema('10s'))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
