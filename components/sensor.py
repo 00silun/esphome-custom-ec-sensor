ce1e916677fb8ce1e6d64268d15df8f074096f24
00silun <80539080+00silun@users.noreply.github.com>
2025-02-24 21:43:26 +0100

    workaround to alias the import so that the sensor module isn’t immediately evaluated.

--- components/sensor.py
+++ components/sensor.py
@@ -1,16 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
+from esphome.components import sensor as sensor_component
 from esphome.const import CONF_ID
 
 # Create a namespace matching your C++ code.
 custom_ec_sensor_ns = cg.esphome_ns.namespace('custom_ec_sensor')
 
-# Declare your C++ class. Make sure "EcSensor" exactly matches your class name.
-EcSensor = custom_ec_sensor_ns.class_('EcSensor', cg.PollingComponent, sensor.Sensor)
+# Declare your C++ class. Ensure "EcSensor" exactly matches your header.
+EcSensor = custom_ec_sensor_ns.class_('EcSensor', cg.PollingComponent, sensor_component.Sensor)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(EcSensor),
-    cv.Required("ads_sensor"): cv.use_id(sensor.Sensor),
-    cv.Required("water_temperature"): cv.use_id(sensor.Sensor),
-}).extend(sensor.sensor_schema(EcSensor))
+    cv.Required("ads_sensor"): cv.use_id(sensor_component.Sensor),
+    cv.Required("water_temperature"): cv.use_id(sensor_component.Sensor),
+}).extend(sensor_component.sensor_schema(EcSensor))
